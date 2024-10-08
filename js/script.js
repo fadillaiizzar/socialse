34d5498ced33b58d1b6cc5a4fbae9192cade4d72
fadillaiizzar <fadillaiizzarm@gmail.com>
2024-10-08 22:05:46 +0700

    five

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const mobileMenu = document.getElementById('mobile-menu');
     hamburger.addEventListener('click', () => {
         mobileMenu.classList.toggle('hidden');
     });
+
+const btndark = document.getElementById('btndark');
+
+    btndark.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
